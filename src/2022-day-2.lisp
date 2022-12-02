5b0abbd06379c08f775555288eec681e631a783f
Edward John Steere <edward.steere@gmail.com>
2022-12-02 06:51:48 +0200

    Fix the `with-open-file' call

--- src/2022-day-2.lisp
+++ src/2022-day-2.lisp
@@ -4,11 +4,11 @@
 (in-package 2022-day-2)
 
 (defun part-1 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname "src/2022-day-2.in"))
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-2.in"))
                  )))
     ))
 
 (defun part-2 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname "src/2022-day-2.in"))
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-2.in"))
                  )))
     ))
