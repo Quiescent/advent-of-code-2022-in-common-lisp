58069943c5ec9449742d72fabaab1f118fa758ce
Edward John Steere <edward.steere@gmail.com>
2022-12-06 07:08:03 +0200

    Solve day 6

--- src/2022-day-6.lisp
+++ src/2022-day-6.lisp
@@ -4,11 +4,22 @@
 (in-package 2022-day-6)
 
 (defun part-1 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-6.in"))
-                 )))
-    ))
+  (let ((line (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-6.in"))
+                (coerce (read-line f nil nil) 'list))))
+    (iter
+      (for char in line)
+      (for p-char previous char)
+      (for pp-char previous p-char)
+      (for ppp-char previous pp-char)
+      (for i from 0)
+      (when (and char p-char pp-char ppp-char
+                 (= (length (remove-duplicates (list char p-char pp-char ppp-char))) 4))
+        (return (1+ i))))))
 
 (defun part-2 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-6.in"))
-                 )))
-    ))
+  (let ((line (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-6.in"))
+                (coerce (read-line f nil nil) 'list))))
+    (iter
+      (for i from 0)
+      (when (= (length (remove-duplicates (subseq line i (+ i 14)))) 14)
+        (return (+ i 14))))))
