4a2b6c0172fd50314a93251fbfe79936c84f01f3
Edward John Steere <edward.steere@gmail.com>
2022-12-07 07:29:16 +0200

    Solve day 7

--- src/2022-day-7.lisp
+++ src/2022-day-7.lisp
@@ -5,10 +5,64 @@
 
 (defun part-1 ()
   (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-7.in"))
-                 )))
-    ))
+                 (do ((line (read-line f nil nil) (read-line f nil nil))
+                      (graph (make-hash-table :test #'equal))
+                      (pwd (list "/"))
+                      (listing nil))
+                     ((null line) graph)
+                   (match line
+                     ((ppcre "\\$ cd (.*)"
+                             where)
+                      (cond
+                        ((string-equal "/" where) (setf pwd (list "/")))
+                        ((string-equal ".." where) (pop pwd))
+                        (t  (push where pwd)))
+                      (setf listing nil))
+                     ((ppcre "\\$ ls")
+                      (setf listing t))
+                     ((ppcre "(\\d+) (\\w+)"
+                             (read file-size)
+                             (read file-name))
+                      (iter
+                        (for subdir on pwd)
+                        (incf (gethash subdir graph 0) file-size)))
+                     ((ppcre "dir (\\w+)"
+                             (read dir-name))
+                      nil))))))
+    (iter
+      (for (key size) in-hashtable input)
+      (when (<= size 100000)
+        (summing size)))))
 
 (defun part-2 ()
   (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-7.in"))
-                 )))
-    ))
+                 (do ((line (read-line f nil nil) (read-line f nil nil))
+                      (graph (make-hash-table :test #'equal))
+                      (pwd (list "/"))
+                      (listing nil))
+                     ((null line) graph)
+                   (match line
+                     ((ppcre "\\$ cd (.*)"
+                             where)
+                      (cond
+                        ((string-equal "/" where) (setf pwd (list "/")))
+                        ((string-equal ".." where) (pop pwd))
+                        (t  (push where pwd)))
+                      (setf listing nil))
+                     ((ppcre "\\$ ls")
+                      (setf listing t))
+                     ((ppcre "(\\d+) (\\w+)"
+                             (read file-size)
+                             (read file-name))
+                      (iter
+                        (for subdir on pwd)
+                        (incf (gethash subdir graph 0) file-size)))
+                     ((ppcre "dir (\\w+)"
+                             (read dir-name))
+                      nil))))))
+    (let* ((available (- 70000000 (gethash (list "/") input)))
+           (required (- 30000000 available)))
+      (iter
+        (for (key size) in-hashtable input)
+        (when (>= size required)
+          (minimizing size))))))
