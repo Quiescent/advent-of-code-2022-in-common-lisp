4bfadb52f5182a9c81b29515761661a5fa509de8
Edward Steere <edward.steere@gmail.com>
2022-12-14 06:31:32 +0200

    Add functions for day 14

--- src/2022-day-14.lisp
+++ src/2022-day-14.lisp
@@ -3,3 +3,12 @@
   (:shadowing-import-from :arrow-macros :->>))
 (in-package 2022-day-14)
 
+(defun part-1 ()
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-14.in"))
+                 )))
+    ))
+
+(defun part-2 ()
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-14.in"))
+                 )))
+    ))
