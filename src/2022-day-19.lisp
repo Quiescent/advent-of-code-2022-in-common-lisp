0ab5524dc92ed9c9e5add135f61bca53f1fb7d05
Edward John Steere <edward.steere@gmail.com>
2022-12-19 07:32:32 +0200

    Wait, then construct

--- src/2022-day-19.lisp
+++ src/2022-day-19.lisp
@@ -37,15 +37,18 @@
                    obsidian-clay-cost
                    geode-ore-cost
                    geode-obsidian-cost)
-  (labels ((recur (ores
+  (labels ((recur (minute
+                   ores
                    clays
                    obsidians
                    geodes
                    ore
                    clay
                    obsidian
+                   geode
                    under-construction)
              (declare (type fixnum
+                            minute
                             ores
                             clays
                             obsidians
@@ -53,9 +56,106 @@
                             ore
                             clay
                             obsidian
+                            geode
                             under-construction))
-             ))
-    (recur 1 0 0 0 0 0 0 -1)))
+             (let ((next-ore (+ ore ores))
+                   (next-clay (+ clay clays))
+                   (next-obsidian (+ obsidian obsidians))
+                   (next-geode (+ geode geodes))
+                   (next-ores (if (= under-construction 0)
+                                  (1+ ores)
+                                  ores))
+                   (next-clays (if (= under-construction 1)
+                                   (1+ clays)
+                                   clays))
+                   (next-obsidians (if (= under-construction 2)
+                                       (1+ obsidians)
+                                       obsidians))
+                   (next-geodes (if (= under-construction 3)
+                                    (1+ geodes)
+                                    geodes)))
+               (format t "(list minute
+                            ores
+                            clays
+                            obsidians
+                            geodes
+                            ore
+                            clay
+                            obsidian
+                            under-construction): ~a~%" (list minute
+                                                             ores
+                                                             clays
+                                                             obsidians
+                                                             geodes
+                                                             ore
+                                                             clay
+                                                             obsidian
+                                                             next-geode
+                                                             under-construction))
+               (if (= minute 24)
+                   (print geodes)
+                   (max
+                    (if (and (>= ore geode-ore-cost)
+                             (>= obsidian geode-obsidian-cost))
+                        (recur (1+ minute)
+                               next-ores
+                               next-clays
+                               next-obsidians
+                               next-geodes
+                               (- next-ore geode-ore-cost)
+                               next-clay
+                               (- next-obsidian geode-obsidian-cost)
+                               next-geode
+                               3)
+                        most-negative-fixnum)
+                    (if (and (>= ore obsidian-ore-cost)
+                             (>= ore obsidian-clay-cost))
+                        (recur (1+ minute)
+                               next-ores
+                               next-clays
+                               next-obsidians
+                               next-geodes
+                               (- next-ore obsidian-ore-cost)
+                               (- next-clay obsidian-clay-cost)
+                               next-obsidian
+                               next-geode
+                               2)
+                        most-negative-fixnum)
+                    (if (>= ore clay-ore-cost)
+                        (recur (1+ minute)
+                               next-ores
+                               next-clays
+                               next-obsidians
+                               next-geodes
+                               (- next-ore clay-ore-cost)
+                               next-clay
+                               next-obsidian
+                               next-geode
+                               1)
+                        most-negative-fixnum)
+                    (if (>= ore ore-ore-cost)
+                        (recur (1+ minute)
+                               next-ores
+                               next-clays
+                               next-obsidians
+                               next-geodes
+                               (- next-ore ore-ore-cost)
+                               next-clay
+                               next-obsidian
+                               next-geode
+                               0)
+                        most-negative-fixnum)
+                    (recur (1+ minute)
+                           next-ores
+                           next-clays
+                           next-obsidians
+                           next-geodes
+                           next-ore
+                           next-clay
+                           next-obsidian
+                           next-geode
+                           -1))))))
+    (recur 1 1 0 0 0 0 0 0 0 -1)))
 
 (defun file-lines (f)
   (do ((line (read-line f nil nil) (read-line f nil nil))
