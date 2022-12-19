1f1a82210775f1f10af8c42138b7698d7dd77762
Edward John Steere <edward.steere@gmail.com>
2022-12-19 06:55:45 +0200

    Add a read-file function to day 19 in case

--- src/2022-day-19.lisp
+++ src/2022-day-19.lisp
@@ -5,8 +5,14 @@
 
 (defun part-1 ()
   (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-19.in"))
-                 )))
-    ))
+                 (file-lines f))))
+    input))
+
+(defun file-lines (f)
+  (do ((line (read-line f nil nil) (read-line f nil nil))
+       (lines))
+      ((null line) (nreverse lines))
+    (push line lines)))
 
 (defun part-2 ()
   (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-19.in"))
