98f63a63c9c741650e932fdaf7277038a1a9a0f4
Edward John Steere <edward.steere@gmail.com>
2022-12-20 09:56:18 +0200

    Solve day 20

--- src/2022-day-20.lisp
+++ src/2022-day-20.lisp
@@ -1,14 +1,49 @@
 (defpackage 2022-day-20
   (:use :cl :iterate :cl-ppcre :metabang-bind :trivia :trivia.ppcre)
-  (:shadowing-import-from :arrow-macros :->>))
+  (:shadowing-import-from :arrow-macros :->>)
+  (:shadowing-import-from :arrow-macros :->))
 (in-package 2022-day-20)
 
 (defun part-1 ()
   (bind ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-20.in"))
                   (input-lines f)))
          ((order . reverse-mapping) (symbols-for-numbers input)))
-    (numbers-for-symbols (mix order reverse-mapping)
-                         reverse-mapping)))
+    (apply #'+
+           (coordinates (mix-2 order order reverse-mapping)
+                        reverse-mapping))))
+
+;; Wrong: 8494
+
+(defun coordinates (order reverse-mapping)
+  (let* ((zero (iter
+                 (for (s value) in-hashtable reverse-mapping)
+                 (when (= value 0)
+                   (return s))))
+         (zero-pos (position zero order))
+         (numbers (numbers-for-symbols order reverse-mapping))
+         (len (length order)))
+    (print (list (nth (mod (+ zero-pos 1000) len) numbers)
+                 (nth (mod (+ zero-pos 2000) len) numbers)
+                 (nth (mod (+ zero-pos 3000) len) numbers)))))
+
+#+nil
+(iter
+  (with len = (length order))
+  (with zero =
+        (iter
+          (for (s value) in-hashtable reverse-mapping)
+          (when (= value 0)
+            (return s))))
+  (with d-numbers = (from-list order))
+  (for s = (d-list-value d-numbers))
+  (while (not (eq s zero)))
+  (setf d-numbers (forward 1 d-numbers))
+  (finally
+   (return
+     (let ((numbers (numbers-for-symbols (to-list d-numbers) reverse-mapping)))
+       (list (nth (mod 1000 len) numbers)
+             (nth (mod 2000 len) numbers)
+             (nth (mod 3000 len) numbers))))))
 
 (defun symbols-for-numbers (xs)
   (iter
@@ -25,41 +60,47 @@
     (for s in ss)
     (collecting (gethash s reverse-mapping))))
 
-(defun mix (order reverse-map)
+(defun mix-2 (order original-order reverse-map)
+  (iter
+    (with xs = (copy-seq order))
+    (with len = (length order))
+    (for next in original-order)
+    (for idx = (position next xs))
+    (for without-next = (concatenate 'list
+                                     (subseq xs 0 idx)
+                                     (subseq xs (1+ idx))))
+    (for amount = (gethash next reverse-map))
+    (for new-idx = (mod (+ idx amount)
+                        (1- len)))
+    (if (= new-idx 0)
+        (setf xs (cons next without-next))
+        (setf xs (concatenate 'list
+                              (subseq without-next 0 new-idx)
+                              (list next)
+                              (subseq without-next new-idx))))
+    (finally (return xs))))
+
+(defun mix (order original-order reverse-map)
   (iter
     (with d-numbers = (from-list order))
-    (for next in order)
-    (format t "next: ~a~%" next)
+    (for next in original-order)
     (for amount = (gethash next reverse-map))
-    (format t "amount: ~a~%" amount)
     (iter
       (for s = (d-list-value d-numbers))
       (while (not (eq s next)))
-      (summing 1 into offset)
       (setf d-numbers (forward 1 d-numbers)))
-    (summing amount into offset)
+    (setf d-numbers (d-list-pop d-numbers))
     (setf d-numbers (if (< amount 0)
                         (back (1+ (abs amount)) d-numbers)
-                        (forward amount d-numbers)))
+                        (forward (1- amount) d-numbers)))
     (setf d-numbers (d-list-insert d-numbers next))
-    (setf d-numbers (forward 1 d-numbers))
     (iter
       (for s = (d-list-value d-numbers))
       (while (not (eq s next)))
-      (summing 1 into offset)
       (setf d-numbers (forward 1 d-numbers)))
-    (print-d-list d-numbers)
-    (format t "Popping: ~a~%" (d-list-value d-numbers))
-    (print-d-list d-numbers)
-    ;; (let ((*print-circle* t))
-    ;;   (print d-numbers))
-    (summing -1 into offset)
-    (setf d-numbers (d-list-pop d-numbers))
-    (print-d-list d-numbers)
-    (format t "After a round: ~a~%~%" (numbers-for-symbols (to-list d-numbers)
-                                                         reverse-map))
+    (print (numbers-for-symbols (to-list d-numbers) reverse-map))
     (finally
-     (return (to-list (forward offset d-numbers))))))
+     (return (to-list d-numbers)))))
 
 (defstruct d-list
   (value nil)
@@ -128,6 +169,22 @@
     (push (read-from-string line) lines)))
 
 (defun part-2 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-20.in"))
-                 )))
-    ))
+  (bind ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-20.in"))
+                  (mapcar (lambda (x) (* x 811589153))
+                          (input-lines f))))
+         ((order . reverse-mapping) (symbols-for-numbers input)))
+    ;; (mix-2 order reverse-mapping)
+    (apply #'+
+           (coordinates (-> (mix-2 order order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping)
+                          (mix-2 order reverse-mapping))
+                        reverse-mapping))))
+
+;; Wrong: 6293062292362
