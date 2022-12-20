3a55c23b4d291109d0c38272c72bf8a947d2f9b5
Edward John Steere <edward.steere@gmail.com>
2022-12-20 06:34:03 +0200

    Add a placeholder for day 20

---
+++ src/2022-day-20.lisp
@@ -0,0 +1,14 @@
+(defpackage 2022-day-20
+  (:use :cl :iterate :cl-ppcre :metabang-bind :trivia :trivia.ppcre)
+  (:shadowing-import-from :arrow-macros :->>))
+(in-package 2022-day-20)
+
+(defun part-1 ()
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-20.in"))
+                 )))
+    ))
+
+(defun part-2 ()
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-20.in"))
+                 )))
+    ))
