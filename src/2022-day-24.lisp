110259102f205a5ac662d692b90c772c17d16a20
Edward Steere <edward.steere@gmail.com>
2022-12-24 06:25:07 +0200

    Fix a typo in the placeholder

--- src/2022-day-24.lisp
+++ src/2022-day-24.lisp
@@ -15,6 +15,6 @@
     (push line lines)))
 
 (defun part-2 ()
-  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "scr/2022-day-24.in"))
+  (let ((input (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-24.in"))
                  )))
     ))
