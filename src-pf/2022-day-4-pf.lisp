08f7c79fc7f5b12580aa3fb0e6e8d98e69f39b81
Edward Steere <edward.steere@gmail.com>
2023-01-14 13:58:36 +0200

    Solve day 4 in functional style

--- src-pf/2022-day-4-pf.lisp
+++ src-pf/2022-day-4-pf.lisp
@@ -2,3 +2,50 @@
   (:use :cl :iterate :cl-ppcre :metabang-bind :trivia :trivia.ppcre)
   (:shadowing-import-from :arrow-macros :->>))
 (in-package 2022-day-4-pf)
+
+(defun part-1 ()
+  (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-4.in"))
+    (labels ((recur (acc)
+               (bind ((line (read-line f nil nil)))
+                 (if (null line)
+                     acc
+                     (recur (+ acc
+                               (match line
+                                 ((ppcre "(\\d+)-(\\d+),(\\d+)-(\\d+)"
+                                         (read a)
+                                         (read b)
+                                         (read c)
+                                         (read d))
+                                  (if (or (and (>= a c)
+                                               (<= b d))
+                                          (and (>= c a)
+                                               (<= d b)))
+                                      1
+                                      0)))))))))
+      (recur 0))))
+
+
+(defun part-2 ()
+  (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-4.in"))
+    (labels ((recur (acc)
+               (bind ((line (read-line f nil nil)))
+                 (if (null line)
+                     acc
+                     (recur (+ acc
+                               (match line
+                                 ((ppcre "(\\d+)-(\\d+),(\\d+)-(\\d+)"
+                                         (read a)
+                                         (read b)
+                                         (read c)
+                                         (read d))
+                                  (if (or (and (>= a c)
+                                               (<= a d))
+                                          (and (>= b c)
+                                               (<= b d))
+                                          (and (>= c a)
+                                               (<= c b))
+                                          (and (>= d a)
+                                               (<= d b)))
+                                      1
+                                      0)))))))))
+      (recur 0))))
