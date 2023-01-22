496a02d9ffa224d1373376b658c9b103a03a725a
Edward Steere <edward.steere@gmail.com>
2023-01-22 14:29:47 +0200

    Solve part 2 day 7 in functional style

--- src-pf/2022-day-7-pf.lisp
+++ src-pf/2022-day-7-pf.lisp
@@ -14,40 +14,58 @@
 
 (defun part-1 ()
   (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-7.in"))
-    (labels ((total (hierarchy)
-               (reduce (lambda (acc key value)
-                         (declare (ignore key))
-                         (if (<= value 100000)
-                             (+ acc value)
-                             acc))
-                       hierarchy
-                       :initial-value 0))
-             (accumulate (hierarchy path size)
-               (if (empty? path)
+    (reduce (lambda (acc key value)
+              (declare (ignore key))
+              (if (<= value 100000)
+                  (+ acc value)
+                  acc))
+            (read-file-system f)
+            :initial-value 0)))
+
+(defun read-file-system (f)
+  (labels ((accumulate (hierarchy path size)
+             (if (empty? path)
+                 hierarchy
+                 (accumulate (with hierarchy path (+ (@ hierarchy path) size))
+                             (less-last path)
+                             size)))
+           (process (hierarchy path)
+             (bind ((line (read-line f nil nil)))
+               (if (null line)
                    hierarchy
-                   (accumulate (with hierarchy path (+ (@ hierarchy path) size))
-                               (less-last path)
-                               size)))
-             (process (hierarchy path)
-               (bind ((line (read-line f nil nil)))
-                 (if (null line)
-                     (total hierarchy)
-                     (match line
-                       ((ppcre "\\$ cd (.*)"
-                               directory)
-                        (cond
-                          ((equal directory "/")
-                           (process hierarchy (seq "/")))
-                          ((equal directory "..")
-                           (process hierarchy (less-last path)))
-                          (t
-                           (process hierarchy (with-last path directory)))))
-                       ((ppcre "\\$ ls")
-                        (process hierarchy path))
-                       ((ppcre "dir .*")
-                        (process hierarchy path))
-                       ((ppcre "([0-9]+) .*"
-                               (read size))
-                        (process (accumulate hierarchy path size)
-                                 path)))))))
-      (process (empty-map 0) (seq "/")))))
+                   (match line
+                     ((ppcre "\\$ cd (.*)"
+                             directory)
+                      (cond
+                        ((equal directory "/")
+                         (process hierarchy (seq "/")))
+                        ((equal directory "..")
+                         (process hierarchy (less-last path)))
+                        (t
+                         (process hierarchy (with-last path directory)))))
+                     ((ppcre "\\$ ls")
+                      (process hierarchy path))
+                     ((ppcre "dir .*")
+                      (process hierarchy path))
+                     ((ppcre "([0-9]+) .*"
+                             (read size))
+                      (process (accumulate hierarchy path size)
+                               path)))))))
+    (process (empty-map 0) (seq "/"))))
+
+(defun part-2 ()
+  (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2022-in-common-lisp "src/2022-day-7.in"))    
+    (bind ((file-system (read-file-system f))
+           (used (@ file-system (seq "/")))
+           (capacity 70000000)
+           (required 30000000)
+           (free (- capacity used))
+           (to-free (- required free)))
+      (reduce (lambda (acc key value)
+                (declare (ignore key))
+                (if (and (>= value to-free)
+                         (< value acc))
+                    value
+                    acc))
+              file-system
+              :initial-value used))))
